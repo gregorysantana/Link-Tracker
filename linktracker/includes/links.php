96440b92c1c32f25d486cc7a8ee35691fc0a653a
Marcos Raudkett <info@marcosraudkett.com>
2017-01-10 15:04:24 +0200

    Add files via upload

--- linktracker/includes/links.php
+++ linktracker/includes/links.php
@@ -14,13 +14,13 @@
 				//if we find rows
 				while($row = mysql_fetch_assoc($get_links)) {
 					echo '<div class="well">';
-					echo '<p>Link: <b>'.$row["slt_link_baseurl"].'</b></p>';
+					echo '<p>Link: <b>'.$row["slt_link_baseurl"].' <a class="btn btn-default btn-xs" target="_blank" href="'.$row["slt_link_baseurl"].'">Visit</a></b></p>';
 					echo '<p>Tracking Link: <b>'.$row["slt_link_url"].'</b></p>';
-					echo '<p>Total Visits: <b>'.$row["slt_link_total"].'</b></p>';
-					echo '<a style="display:inline;" class="btn btn-info btn-xs" href="'.$currentpage.'?src='.$row["slt_link_trackingid"].'">Visitors</a>&nbsp;';
+					echo '<p>Total Unique Visitors: <b>'.$row["slt_link_total"].'</b></p>';
+					echo '<a style="display:inline;" class="btn btn-info btn-xs" href="'.$currentpage.'?src='.$row["slt_link_trackingid"].'">Stats</a>&nbsp;';
 					echo '<form style="display:inline;" method="POST" action="includes/delete_link.php">
 					<input type="hidden" name="tid" value="'.$row["slt_link_trackingid"].'">
-					<button style="font-size:9px;" class="btn btn-danger btn-xs" type="submit" value="delete">Delete</button></form>';
+					<button style="font-size:13px;" class="btn btn-danger btn-xs" type="submit" value="delete"><i class="fa fa-trash" aria-hidden="true"></i></button></form>';
 					echo '</div>';
 				}
 			} else {

d7e81a8228e2289c7ee85290485a408d9d94dd1b
Marcos Raudkett <info@marcosraudkett.com>
2017-01-10 15:04:10 +0200

    Add files via upload

--- linktracker/mypanel.php
+++ linktracker/mypanel.php
@@ -60,7 +60,7 @@ if(isset($_COOKIE['usr'])){
 
       ?>
 <div style="margin-top:75px;background:white;border:1px solid grey; width:750px;border-radius: 4px;" class="options">
-      
+      <legend>Active Links <a class="btn btn-default btn-xs" onclick="tableview()" id="tbl" href="javascript:void(0);"><i class="fa fa-table" aria-hidden="true"></i> Table View</a><a style="display:none;" class="btn btn-default btn-xs" onclick="listview()" id="liv" href="javascript:void(0);"><i class="fa fa-table" aria-hidden="true"></i> List View</a></legend> 
       <?php 
 
          if($track == '') {
@@ -70,27 +70,64 @@ if(isset($_COOKIE['usr'])){
         } else {
 
           $get_tracking = mysql_query("SELECT * FROM tracking WHERE slt_tracking_trackid = '$track' LIMIT 0, 25");
+          $get_tracking_table = mysql_query("SELECT * FROM tracking WHERE slt_tracking_trackid = '$track' LIMIT 0, 125");
           $get_links = mysql_query("SELECT * FROM links WHERE slt_link_userid = '$userid'");
           $get_link = mysql_fetch_assoc($get_links);
           $get_tracking_amount = mysql_num_rows($get_tracking);
             echo '<h4>Link:</h4> <p style="font-style:italic;">'.$get_link["slt_link_baseurl"].'</p>';
-            echo '<h4>Tracking:</h4> <p style="font-style:italic;">'.$get_link["slt_link_url"].'</p><br><br>';
-            echo '<b>Total:</b> '.$get_tracking_amount.'<br><br>';
+            echo '<h4>Tracking:</h4> <p style="font-style:italic;">'.$get_link["slt_link_url"].'</p><br>';
+            echo '<b>Unique Visitors:</b> '.$get_tracking_amount.'<br><br>';
 
+          echo '<div id="listview">';
           while($row = mysql_fetch_assoc($get_tracking)) {
-            echo '<div style="text-align:left;">';
-            echo '<p><b>IP Address: </b>'.$row["slt_tracking_ipaddr"].'</p>';
+            echo '<div class="well" style="text-align:left;">';
+            echo '<p><b>IP Address: </b>'.$row["slt_tracking_ipaddr"].' <a class="btn btn-warning btn-xs" target="_blank" href="http://ip-api.com/#'.$row["slt_tracking_ipaddr"].'">Lookup <i style="font-size: 10px;" class="fa fa-external-link" aria-hidden="true"></i></a></p>';
+          if($row["slt_tracking_useragent"] == '') { 
+            echo '<p style="color:#a52424;"><b>Unresolved useragent.</b></p>';
+          } else {
             echo '<p><b>Useragent: </b>'.$row["slt_tracking_useragent"].'</p>';
-            echo '<p><b>Referral:</b> '.$row["slt_tracking_referral"].'</p>';
+          }
+          if($row["slt_tracking_referral"] == '') {
+            echo '<p style="color:#a52424;"><b>Possibly a Direct Hit</b></p>';
+          } else {
+            echo '<p><b>Referral:</b> '.$row["slt_tracking_referral"].' <a class="btn btn-default btn-xs" target="_blank" href="'.$row["slt_tracking_referral"].'">Visit</a> <a class="btn btn-default btn-xs" target="_blank" href="https://whois.domaintools.com/'.$row["slt_tracking_referral"].'">Whois</a></p>';            
+          }
             echo '<p><b>Vsisit Time: </b>'.$row["slt_tracking_time"].'</p>';
             echo '</div>';
         }
+        echo '</div>';
+         echo '<div style="display:none;" id="tableview">';
+          echo '<table class="table">';
+          echo '<thead>';
+          echo '<tr>
+                  <th>IP Address</th>
+                  <th>Referral</th>
+                  <th>Visit Time</th>
+                </tr>
+                <tr>
+                  <tbody>';
+          while($rows = mysql_fetch_assoc($get_tracking_table)) {
+            echo '<td>'.$rows["slt_tracking_ipaddr"].' 
+            <a class="btn btn-warning btn-xs" target="_blank" href="http://ip-api.com/#'.$rows["slt_tracking_ipaddr"].'">Lookup <i style="font-size: 10px;" class="fa fa-external-link" aria-hidden="true"></i></a></td>';
+          
+          if($rows["slt_tracking_referral"] == '') {
+            echo '<td style="color:#a52424;"><b>Possibly a Direct Hit</b></td>';
+          } else {
+
+            echo '<td style="max-width:100px;">'.substr($rows["slt_tracking_referral"],0, 21).' <br><a class="btn btn-default btn-xs" target="_blank" href="'.$rows["slt_tracking_referral"].'">Visit</a> <a class="btn btn-default btn-xs" target="_blank" href="https://whois.domaintools.com/'.$rows["slt_tracking_referral"].'">Whois</a></td>';            
+          }
+            echo '<td>'.$rows["slt_tracking_time"].'</td>';
+          echo '</tbody></tr>';
+        }
+          echo '</table>';
+      echo '</div>';
+
       }
 
       ?>
 
 </div>
-
+<br><br><br>
 
 
 
@@ -99,7 +136,25 @@ if(isset($_COOKIE['usr'])){
 </footer>
 
 
+<script type="text/javascript">
+function tableview() {
+   $("#listview").slideUp("mypanel.php #listview");
+   $("#tbl").hide("mypanel.php #tbl");
+   $("#liv").show("mypanel.php #liv");
+   $("#tableview").slideDown("mypanel.php #tableview");
+}
+function listview() {
+   $("#listview").slideDown("mypanel.php #listview");
+   $("#liv").hide("mypanel.php #liv");
+   $("#tbl").show("mypanel.php #tbl");
+   $("#tableview").slideUp("mypanel.php #tableview");
+}
+</script>
+
 </center>
+<!-- LOAD ANIM -->
+<script src="http://mvrclabs.info/se/labcode/third/jQuery-2.1.4.min.js"></script>
+<script src="http://mvrclabs.info/se/labcode/showhide.js"></script>
 <!-- JS -->
 <script src="/<?php echo $app_path; ?>js/jquery-1.10.2.js"></script>
 <script src="/<?php echo $app_path; ?>js/jquery-ui.js"></script>

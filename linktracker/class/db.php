feb8c172d1aba9f7813bd697f8edade5bbae3070
Marcos Raudkett <info@marcosraudkett.com>
2017-09-03 02:50:45 +0300

    Update db.php

--- linktracker/class/db.php
+++ linktracker/class/db.php
@@ -2,7 +2,7 @@
 	
 	include ("config.php");
 
-	//connetction to the database (change to whatever you'd like.)
+	//connetction to the database (change to whatever you'd like or use web installer)
 		$conn = mysql_connect($db_host, $db_user, $db_pass) or die(mysql_error()); mysql_select_db($db_name) or die(mysql_error()); 
 	//charset to UTF8
 		mysql_set_charset("utf8", $conn);

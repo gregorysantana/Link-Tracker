b932eab0e60700d786186ff21a317dd784d25c37
Marcos Raudkett <info@marcosraudkett.com>
2017-11-15 14:18:01 +0200

    MySQLi -> delete_link.php

--- linktracker/includes/delete_link.php
+++ linktracker/includes/delete_link.php
@@ -1,27 +1,29 @@
-<?php 
-
-	
-	include ("../class/require.php");
-
-	if(isset($_COOKIE['usr'])){ 
-
+<?php
+  //Include MySQL Database Connection.
+  require_once "../class/require.php";
+if(isset($_COOKIE['usr'])){ 
         $email = $_COOKIE['usr'];  
         $pass = $_COOKIE['key']; 
-        $check = mysql_query("SELECT * FROM users WHERE slt_user_email = '$email'")or die(mysql_error());
-        $email = mysql_real_escape_string($_POST['slt_user_email']);
-        $res=mysql_query("SELECT * FROM users WHERE slt_user_email='$email'");
-
-    while($info = mysql_fetch_array( $check ))   {
-        $user_id = $row["slt_user_id"]; 
-        if ($pass != $info['slt_user_password']) 
-            {           header("Location: ".$app_path); 
-
-            } else { 
+        $check_query = "SELECT * FROM users WHERE slt_user_email = '$email'";
+        $check = mysqli_query($conn, $check_query);
+        $email = mysqli_real_escape_string($conn, $_POST['slt_user_email']);
+        $res_query = "SELECT * FROM users WHERE slt_user_email='$email'";
+        $res = mysqli_query($conn, $res_query);
+        
+      while($info = mysqli_fetch_array( $check ))   {
+        $user_id = $info["slt_user_id"]; 
+        $userid = $info["slt_user_id"]; 
+        if ($pass != $info['slt_user_password']) {           
+          header("Location: ".$app_path); 
+        } else { 
 
 
 	        $tid = $_POST["tid"];
-	        $d = mysql_query("DELETE FROM tracking WHERE slt_tracking_trackid = '$tid'");
-	        $d2 = mysql_query("DELETE FROM links WHERE slt_link_trackingid = '$tid'");
+	        $d_query = "DELETE FROM tracking WHERE slt_tracking_trackid = '$tid'";
+            $d = mysqli_query($conn, $d_query);
+            $d2_query = "DELETE FROM links WHERE slt_link_trackingid = '$tid'";
+            $d2 = mysqli_query($conn, $d2_query);
+
 
 			//Send user back to previous site..
 			header("Location: ".$_SERVER["HTTP_REFERER"]);
